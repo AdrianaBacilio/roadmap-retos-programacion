dbf423b9d04566cccbe805117f6cb315cb6a9881
Adriana Bacilio <160262851+AdrianaBacilio@users.noreply.github.com>
2024-09-26 12:46:42 -0600

    AdrianaBacilio.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/AdrianaBacilio.py
@@ -0,0 +1,29 @@
+# https://www.python.org/ 
+
+# Comentario en una linea 
+
+"""Comentario 
+    de
+    varias
+    lineas"""  
+
+''' Comentario 
+    tambien '''
+
+
+my_variable = "Hola mundo, Python" 
+my_variable = "Nuevo valor de mi variable, en python no existen constantes, todo puede varirar"
+
+my_CONSTANT = "EN MAYUSCULAS NO CAMBIA ESTE VALOR" # por convencion 
+
+my_int= 1
+my_float= 1.5 
+my_bool= True 
+my_bool= False 
+my_string= "Cadena de texto"
+my_other_string= 'Otra cadena de texto'
+
+
+print("¡Hola Python!")
+
+
